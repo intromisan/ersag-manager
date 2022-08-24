a10529ac08e18baa80ce7e41a997f5c5e5b565e6
Serge Najimov <najimovserge@gmail.com>
2022-08-24 11:15:39 +0200

    test change to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { InventoryModule } from './inventory/inventory.module';
 
@@ -11,25 +11,20 @@ import { InventoryModule } from './inventory/inventory.module';
       envFilePath: '.development.env',
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => {
-        const isProduction = configService.get('STAGE') === 'prod';
-
-        return {
-          ssl: isProduction,
-          extra: {
-            ssl: isProduction ? { rejectUnauthorized: false } : null,
-          },
-          type: 'postgres',
-          host: process.env.HOST,
-          port: Number(process.env.DATABASE_PORT),
-          username: process.env.DATABASE_USERNAME,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_NAME,
-          autoLoadEntities: true,
-          synchronize: true,
-        };
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
       },
+      type: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     AuthModule,
     ProductsModule,
